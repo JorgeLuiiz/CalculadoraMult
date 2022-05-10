15428cfc7a9b5b26315a4af95f955e5ff4018e8b
JorgeLuiiz <101126355+JorgeLuiiz@users.noreply.github.com>
2022-05-10 11:01:00 -0300

    Atualizando classe

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -3,13 +3,12 @@ public class Calculadora {
     String num1;
     String num2;
 
-    public CalculadoraMultiplicacao(String num1, String num2) {
+    public Calculadora(String num1, String num2) {
         this.num1 = num1;
         this.num2 = num2;
 
     }
-
-    public String multiplicacao(String numero1, String numero2) {
+    public String multi(String numero1, String numero2) {
         Double numeroMulti1 = Double.parseDouble(numero1);
         Double numeroMulti2 = Double.parseDouble(numero2);
         double multi = numeroMulti1 * numeroMulti2;
